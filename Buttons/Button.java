aa884236ae35cabca010af813b791e1a9c85e3d2
Zain Najfi <116354306+ZainNajfi@users.noreply.github.com>
2024-04-21 11:50:49 -0700

    Add files via upload

---
+++ Buttons/Button.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package task1.Buttons;
+
+/**
+ *
+ * @author faiqa
+ */
+public class Button {
+    public interface Buttons {
+    void render();
+    void onClick();
+}
+}
---
+++ Buttons/HtmlButton.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package task1.Buttons;
+
+import task1.Buttons.Button.Buttons;
+
+/**
+ *
+ * @author faiqa
+ */
+public class HtmlButton {
+    public class HtmlButton implements Buttons {
+
+    public void render() {
+        System.out.println("<button>Test Button</button>");
+        onClick();
+    }
+
+    public void onClick() {
+        System.out.println("Click! Button says - 'Hello World!'");
+    }
+}
+}
